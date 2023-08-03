b8326321862b5fc5f4929001646e87b9532c7151
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-08-03 09:00:31 +0300

    added useEffect hook to display content and fixed some UI problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import diceIcon from "./assets/images/icon-dice.svg";
 import patternDividerDesktop from "./assets/images/pattern-divider-desktop.svg";
 import patternDividerMobile from "./assets/images/pattern-divider-mobile.svg";
@@ -11,19 +11,27 @@ function App() {
     setAdvice(data.slip.advice);
     setId(data.slip.id);
   };
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
     <main className="bg-darkBlue w-full h-full flex justify-center items-center  ">
       <div className="bg-darkGrayishBlue p-5 rounded-lg flex flex-col items-center justify-center max-w-sm sm:max-w-lg">
         <p className="uppercase text-neonGreen tracking-widest">advice #{id}</p>
-        <p className="text-lightCyan mt-5 ">{advice}</p>
-        <div className="mb-14 mt-5 sm:hidden">
+        <p className="text-lightCyan mt-5 text-center text-2xl font-manrope">
+          "{advice}"
+        </p>
+        <div className="mb-7 mt-5 sm:hidden">
           <img src={patternDividerMobile} alt="" />
         </div>
-        <div className="mb-14 mt-5 hidden sm:inline-block">
+        <div className="mb-14 mt-5 hidden sm:inline-block p-4">
           <img src={patternDividerDesktop} alt="" className="" />
         </div>
-        <button className="z-10 -mb-14 overflow-visible bg-neonGreen rounded-full p-5">
-          <img src={diceIcon} alt="" onClick={getAdvice} />
+        <button
+          className="z-10 -mb-14 overflow-visible bg-neonGreen rounded-full p-5"
+          onClick={getAdvice}
+        >
+          <img src={diceIcon} alt="" />
         </button>
       </div>
     </main>
